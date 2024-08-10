44b4f4c966e9717c86cb60322b3a7bf477525594
Ruhail Mir <theksa777@gmail.com>
2024-08-10 10:27:12 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Instructions
+
+1. Install Flask using pip:
+   - pip install Flask
+
+2. For ( Mueed ):
+   - Add some content of your choice to index.html file in the templates folder, make it look good.
+
+3. Run the application:
+   - Open the terminal in the CloudProj folder and type:
+   - python app.py or py app.py
+
+4. Open the generated URL in the browser to view the app
---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.environ.get("GEM_API_KEY")
+
+app = Flask(__name__)
+
+genai.configure(api_key=api_key)
+
+# Initialize the Gemini model
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+
+@app.route("/", methods=["GET", "POST"])
+def summarize():
+    summary = ""
+    if request.method == "POST":
+        text = request.form["text"]
+        if text:
+            response = model.generate_content(f"Summarize the following text: {text}")
+            summary = response.text
+    return render_template("summarize.html", summary=summary)
